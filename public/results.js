a02527b7dcf0caf87ae20c0cefe1a3c07530f79b
System <none>
2017-02-24 17:33:27 +0000

    Visual Design, UI, Results browser

--- public/results.js
+++ public/results.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
   
-  $('#new').submit(function(event) {
-    event.preventDefault();
-    $.post('/teacher', { key: $('#key').val() } )
-      .done(function(data) {
-        
-      });
+  var reload;
+  
+  function autoReload(checked) {
+    if (checked) {
+      clearTimeout(reload);
+    } else {
+      reload = setTimeout("window.location.reload();", 5000);
+    }
+  }
+  
+  $('#view').click(function(event) {
+    $('.left').toggle();
+    $('.complete').toggleClass("view");
+    $('tr:last-child td:nth-child(2)').toggleClass("bottom-left");
+    var checked = this.checked;
+    autoReload(checked);
   });
   
+  autoReload();
 
 });
